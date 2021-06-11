312f2d0b6a2576982ccc3b61111649da274718d8
seif <seifeldinmohamed101@gmail.com>
2021-06-12 00:52:52 +0200

    Intent Service class

--- app/src/main/java/com/seif/services/MyIntentService.kt
+++ app/src/main/java/com/seif/services/MyIntentService.kt
@@ -5,11 +5,11 @@ import android.content.Intent
 import android.util.Log
 
 
-class MyIntentService: IntentService("MyIntentService") {
-   val TAG = "IntentService"
+class MyIntentService : IntentService("MyIntentService") {
+    val TAG = "IntentService"
     override fun onHandleIntent(inte: Intent?) {
-        for (i in 1..5){
-            Log.d(TAG, inte!!.extras!!.get("type").toString()+"the service is doing something $i")
+        for (i in 1..5) {
+            Log.d(TAG, inte!!.extras!!.get("type").toString() + "the service is doing something $i")
             Thread.sleep(1000)
         }
     }
