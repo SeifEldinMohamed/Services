8f3871eab463d71b6f4f9e78ba7e1c23cd4860d3
seif <seifeldinmohamed101@gmail.com>
2021-06-12 03:14:22 +0200

    Services

--- app/src/main/java/com/seif/services/MainActivity.kt
+++ app/src/main/java/com/seif/services/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
-// unbounded(started) service
+// unbounded(started) service:
 //        val intent = Intent(this, MyService::class.java)
 //        binding.startService.setOnClickListener {
 //            startService(intent)
@@ -25,6 +25,7 @@ class MainActivity : AppCompatActivity() {
 //           stopService(intent)
 //       }
 
+        // Intent Service
         val intent = Intent(this, MyIntentService::class.java)
         intent.putExtra("type", TYPEONE)
         startService(intent)
@@ -46,3 +47,31 @@ Caution:
  the user cannot see which service starts. Beginning with Android 5.0 (API level 21),
  the system throws an exception if you call bindService() with an implicit intent.
  */
+
+/*Notice that the onStartCommand() method must return an integer.
+  The integer is a value that describes how the system should continue the service in the event that
+  the system kills it. The return value from onStartCommand() must be one of the following constants:
+*/
+/*
+START_NOT_STICKY
+If the system kills the service after onStartCommand() returns,
+do not recreate the service unless there are pending intents to deliver.
+This is the safest option to avoid running your service when not necessary and
+when your application can simply restart any unfinished jobs.
+*/
+/*
+START_STICKY
+If the system kills the service after onStartCommand() returns,
+recreate the service and call onStartCommand(), but do not redeliver the last intent. Instead,
+the system calls onStartCommand() with a null intent unless there are pending intents to start the service.
+In that case, those intents are delivered.
+This is suitable for media players (or similar services)
+that are not executing commands but are running indefinitely and waiting for a job.
+*/
+/*
+START_REDELIVER_INTENT
+If the system kills the service after onStartCommand() returns,
+recreate the service and call onStartCommand() with the last intent that was delivered to the service.
+Any pending intents are delivered in turn. This is suitable for services that are actively performing
+a job that should be immediately resumed, such as downloading a file.
+*/
--- app/src/main/java/com/seif/services/MyIntentService.kt
+++ app/src/main/java/com/seif/services/MyIntentService.kt
@@ -6,11 +6,26 @@ import android.util.Log
 
 
 class MyIntentService : IntentService("MyIntentService") {
-    val TAG = "IntentService"
-    override fun onHandleIntent(inte: Intent?) {
-        for (i in 1..5) {
-            Log.d(TAG, inte!!.extras!!.get("type").toString() + "the service is doing something $i")
-            Thread.sleep(1000)
+    private val TAG = "IntentService"
+    override fun onHandleIntent(intet: Intent?) {
+        when (intet!!.extras!!.get("type")) {
+            TYPEONE -> {
+                for (i in 1..5) {
+                    Log.d(TAG, "the First service is doing something $i")
+                    Thread.sleep(1000)
+                }
+            }
+            TYPETWO -> {
+                for (i in 1..5) {
+                    Log.d(TAG, "the Second service is doing something $i")
+                    Thread.sleep(1000)
+                }
+            }
         }
     }
 }
+/*
+This class was deprecated in API level 30.
+IntentService is subject to all the background execution limits imposed with Android 8.0 (API level 26).
+Consider using WorkManager or JobIntentService, which uses jobs instead of services when
+running on Android 8.0 or higher.*/
